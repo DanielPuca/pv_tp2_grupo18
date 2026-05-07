484016845d2bdd4ef62c25f05cce81b22ba9d018
Antonella Csongedy <csongedyanto@gmail.com>
2026-05-07 04:20:48 -0300

    Ejercicios 19 y 25 agregados por Antonella Csongedy

--- src/services/serviceEjercicio20.js
+++ src/services/serviceEjercicio20.js
@@ -1,24 +1,28 @@
-export const ObtenerDatosAlumno = () => {
-    const inputNombre = document.querySelector("#nombre");
-    const inputApellido = document.querySelector("#apellido");
-    const inputLU = document.querySelector("#lu");
 
-    const nombreval = inputNombre.value;
-    const apellidoval = inputApellido.value;
-    const luval = inputLU.value;
+export const guardarEstudianteEnArray = (estudiante) => {
 
-    return { nombreval, apellidoval, luval };
-}
+    const listaEstudiantes = [];
+    listaEstudiantes.unshift(estudiante);
+    console.log(listaEstudiantes);
 
-export const CrearFicha = (nombreval, apellidoval, luval) =>{
-    const fichaInfo = document.createElement('div');
+    return (listaEstudiantes);
+};
 
-    fichaInfo.innerHTML = `
-        <h3>Ficha de Alumno</h3>
-        <p><strong>Nombre:</strong> ${nombreval}</p>
-        <p><strong>Apellido:</strong> ${apellidoval}</p>
-        <p><strong>LU:</strong> ${luval}</p>
-    `;
+export const otraFuncion = ()=>{
 
-    return fichaInfo;
-}
+};
+
+export const mostrarEstudiante = (a, b, c)=>{
+    
+        const fichaInfo = document.createElement('div');
+        fichaInfo.className = 'info-box';
+
+        fichaInfo.innerHTML = `
+            <h3>Los datos ingresados son:</h3>
+            <p><strong>Nombre:</strong> ${a}</p>
+            <p><strong>Apellido:</strong> ${b}</p>
+            <p><strong>Libreta Universitaria:</strong> ${c}</p>
+        `;
+
+        return fichaInfo;
+};
